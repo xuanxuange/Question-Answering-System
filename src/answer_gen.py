4a91fd7c1e7cc09bd441ab7987a885fcc645e61f
Xuanxuan Ge <xuanxuag@andrew.cmu.edu>
2020-03-31 12:48:33 -0400

    Fix bug
    Fix punctuation problem and add some sample q/rel_sentece

--- src/answer_gen.py
+++ src/answer_gen.py
@@ -5,6 +5,10 @@ import string
 from spacy.symbols import *
 
 def find_sv(word):
+    """
+    Check if given node is the main subject
+    Return: T if the node is main subject, F otherwise
+    """
     if word.dep == nsubj and (word.head.pos == VERB or word.head.pos == AUX or (len(rights) > 0 and word.head.pos == NOUN and rights[0] == ADP)):
         return True
     else:
@@ -36,6 +40,7 @@ def answer_whn(question, rel_sentence):
             main_v = word.head
             main_subj = word
             main_v_index = q_tokens.index(main_v.text)
+            
     # find matched v in sentence
     for i, word in enumerate(s_dep):
         if word.head == word:
@@ -44,7 +49,7 @@ def answer_whn(question, rel_sentence):
         if word.head.head == word.head and word.lemma_ == main_v.lemma_:
             root_index = i
         
-    # check if main subj is the wh word
+    # check if main subj is the wh word (first word in question)
     if q_tokens[0] == main_subj.text:
         meaningful_pos = [NOUN, VERB, ADJ, ADV, AUX, PROPN]
         flag = True
@@ -79,10 +84,9 @@ def answer_whn(question, rel_sentence):
     flat_answer = [item for sub in answer for item in sub]
 
     # add/change the last element to be period
-    if flat_answer[-1] not in string.punctuation: 
-        flat_answer.append(' .')
-    else:
-        flat_answer[-1] = '.'
+    if flat_answer[-1] not in string.punctuation and flat_answer[-1][-1] not in string.punctuation: 
+        flat_answer.append('.')
+    
     return ' '.join(flat_answer)
 
 def answer_howx(question, rel_sentence):
@@ -111,7 +115,7 @@ def answer_howx(question, rel_sentence):
         return rel_sentence
     if len(answer) == 0:
         return rel_sentence
-    answer.append(' .')
+    answer.append('.')
     return ' '.join(answer)
 
 def answer_where(question, rel_sentence):
@@ -128,7 +132,7 @@ def answer_where(question, rel_sentence):
         if ent.label_ == 'LOC':
             pos_answer.append(ent.text)
     if len(pos_answer) == 1:
-        answer = pos_answer[0] + ' .'
+        answer = pos_answer[0] + '.'
     elif len(pos_answer) < 1:
         return rel_sentence
 
@@ -154,7 +158,7 @@ def answer_where(question, rel_sentence):
                         if len(r) == 0: continue
                         for a in pos_answer:
                             if r[0] in a:
-                                answer = a + ' .'
+                                answer = a + '.'
                                 break       
             if not answer:
                 answer = rel_sentence # or return pos_answer[0]?
@@ -181,7 +185,7 @@ def answer_when(question, rel_sentence):
                 pos_answer[i] = chunk.text
                 break
     if len(pos_answer) == 1:
-        answer = pos_answer[0] + ' .'
+        answer = pos_answer[0] + '.'
     elif len(pos_answer) < 1:
         return rel_sentence
     # > 1 possible answer, return the one closest to the main verb in question
@@ -193,7 +197,7 @@ def answer_when(question, rel_sentence):
         if not main_v or main_v not in rel_sentence:
             return rel_sentence
         
-        answer = find_closest_answer(pos_answer, main_v, rel_sentence) + ' .'
+        answer = find_closest_answer(pos_answer, main_v, rel_sentence) + '.'
         if not answer:
             return rel_sentence
     return answer
@@ -206,7 +210,12 @@ def dfs_tree(curr, label, pos_answer, visited):
     visited.append(curr)
     for child in curr:
         if not isinstance(child, str) and child.label() == label:
-            pos_answer.append(child.leaves())
+            adv = child.leaves()
+            # remove punctuations
+            if adv[-1] in string.punctuation:
+                adv = adv[ : -1]
+            pos_answer.append(adv)
+            
         if child not in visited:
             dfs_tree(child, label, pos_answer, visited)
     return 
@@ -221,8 +230,10 @@ def find_closest_answer(pos_answer, main_v, rel_sentence):
     answer = ''
     for a in pos_answer:
         a_index = rel_sentence.find(a)
-        if abs(a_index - main_v_index) < min_dist:
+        curr_dist = abs(a_index - main_v_index)
+        if curr_dist < min_dist:
             answer = a
+            min_dist = curr_dist
     return answer
 
 def answer_other_adv(question, rel_sentence):
@@ -232,7 +243,7 @@ def answer_other_adv(question, rel_sentence):
     """
     ADV_STOP_LIST = ['almost', 'also', 'further', 'generally', 'greatly',
     'however', 'just', 'later', 'longer', 'often', 'only', 'typically', 
-    'similarly', 'initially', 'for', 'basically', 'already', 'literally']
+    'similarly', 'initially', 'for', 'basically', 'already', 'literally', 'largely']
     nlp = spacy.load("en_core_web_sm") 
     sent = nlp(rel_sentence)
     q = nlp(question)
@@ -242,7 +253,7 @@ def answer_other_adv(question, rel_sentence):
     for word in q:
         if find_sv(word):
             main_v = word.head
-    if (main_v.text not in rel_sentence and main_v.lemma_.text not in rel_sentence) or not main_v:
+    if (main_v.text not in rel_sentence and main_v.lemma_  not in rel_sentence) or not main_v:
         return rel_sentence
     for word in sent:
         if word.head.lemma_ == main_v.lemma_ and word.dep == advmod and word.text.lower() not in ADV_STOP_LIST:
@@ -251,20 +262,23 @@ def answer_other_adv(question, rel_sentence):
             span = str(sent[word.left_edge.i : word.right_edge.i+1])
             answer += span
     if answer:
-        answer += ' .'
+        answer += '.'
     # find the advp in parse tree
     else:
         tree = parse_raw_text(rel_sentence)
         pos_answer = []
         visited = []
         dfs_tree(tree[0], 'ADVP', pos_answer, visited)
+        # flatten pos_answer and remove non-meaningful answer
         pos_answer = [a for sublist in pos_answer for a in sublist]
+        pos_answer = [a for a in pos_answer if a.lower() not in ADV_STOP_LIST]
+
         if not pos_answer:
             return rel_sentence
-        elif len(pos_answer) == 1 and pos_answer[0].lower() not in ADV_STOP_LIST:
-            answer = pos_answer[0].lower() + ' .'
+        elif len(pos_answer) == 1:
+            answer = pos_answer[0].lower() + '.'
         else:
-            answer = find_closest_answer(pos_answer, main_v.text, rel_sentence) + ' .'
+            answer = find_closest_answer(pos_answer, main_v.text, rel_sentence) + '.'
 
     if not answer:
         return rel_sentence
@@ -295,9 +309,27 @@ def answer_why(question, rel_sentence):
 
 if __name__ == "__main__":
     # question = "What is the primary weapon of Egyptian armies during the new Kingdom ?"
-    question = "What leads to the death of the princess ?"
-    sent = "Bow and arrow was the principal weapon of the Egyptian army ;"
+    # question = "What leads to the death of the princess ?"
+    # question = "How many people are there in the room ?"
+    # question = "How much does it take for you to finish the task ?"
+    # question = "What is the Egyptian Empire ?"
+    # question = "Where was the tomb for sons of Ramesses II?"
+    # question = "Where is the Osirian temple?"
+    # sent = "the tomb he built for his sons, many of whom he outlived, in the Valley of the Kings has proven to be the largest funerary complex in Egypt ."
+    # sent = "In the Osirian temple at Denderah, an inscription (translated by Budge, Chapter XV, Osiris and the Egyptian Resurrection) describes in detail the making of wheat paste models of each dismembered piece of Osiris to be sent out to the town where each piece is discovered by Isis."
+    # sent = "It took me $100,000 to finish ."
+    # sent = "There are one hundred people in the room."
+    # sent = "Bow and arrow was the principal weapon of the Egyptian army;"
     # sent = "The principal weapon of the Egyptian army was bow and arrow."
     # sent = "The weapon leads to the death of the princess."
     # sent = "The main thing that leads to the death of the princess was apple."
-    answer_whn(question, sent)
+    # sent = "The apple leads to the death of the princess ."
+    sent = "The 4th-6th Dynasties of Egypt, are scarce and historians regard the history of the era as literally 'written in stone' and largely architectural in that it is through the monuments and their inscriptions peacefully that scholars have been able to construct a history."
+    question = "How did the scholars construct a history of the 4th-6th Dynasties of Egypt ?"
+    # question = "How did he complete the task? "
+    # sent = "Under the bridge, he completed the task peacefully under her help."
+    # question = "When did the Old Kingdom and its power reach a zenith ?"
+    # sent = "The Old Kingdom and its royal power reached a zenith under the Fourth Dynasty (26132494 BC), which began with Sneferu (26132589 BC)."
+    print(answer_whadv(question, sent))
+    # print(answer_whn(question, sent))
+    # print(answer_howx(question, sent))
