aaca824efbd71db75e66e5e1f39dab2358794d0e
dudongtai2002 <dudongtai2002@gmail.com>
2020-04-11 04:00:42 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 SHARE=$(pwd)
 IMAGE=$1
-NAME=$(sudo docker run -d -p 9000:9000 -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
+NAME=$(sudo docker run -d -v ${SHARE}:/host/Users -it ${IMAGE} /bin/bash)
 echo '****************'
-sudo docker exec -it $NAME ./ask /host/handout-dryrun/data/set1/a1.txt 3
+sudo docker exec -it $NAME ./ask /host/Users/handout-dryrun/data/set1/a1.txt 3
 echo '****************'
 sudo docker exec -it $NAME ./answer /host/Users/handout-dryrun/data/set1/a1.txt /host/Users/test_questions.txt
 echo '****************'
